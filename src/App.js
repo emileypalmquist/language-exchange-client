abac1f3a4c1aeeb023b78088d971b4cd8e5d5178
github username <github email address>
2019-12-03 16:05:49 -0800

    signup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import Home from './containers/Home'
 import Welcome from './containers/Welcome'
+import Signup from './containers/SignUp';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import './App.css';
 
 class App extends React.Component {
-  state = {
-    user: ''
+  constructor() {
+    super()
+
+    this.state = {
+      user: null,
+    }
+
   }
+
+
   render() {
+    const {user, signUpClick} = this.state
+
     return (
+      
       <div className="App">
-        {this.state.user ? <Home /> : <Welcome />}
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route  path="/signup" component={Signup} />
+            <Route  path="/home" component={Home} />
+            </Switch>
+        </Router>
       </div>
+
     );
   }
 }
-
+ 
 export default App;
